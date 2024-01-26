5dcaa5b294f9b207b7f125400dc006d9d5094e5c
John Shaffer <john@insilica.co>
2024-01-26 15:03:00 -0600

    Download snapshot

--- stages/01_download.sh
+++ stages/01_download.sh
@@ -6,12 +6,12 @@
 localpath=$(pwd)
 echo "Local path: $localpath"
 
-# Create brick directory
-brickpath="$localpath/brick"
-mkdir -p $brickpath
-echo "Brick path: $brickpath"
+# Create download directory
+dlpath="$localpath/download"
+mkdir -p "$dlpath"
+echo "Brick path: $dlpath"
 
 # Retrieve the files
-aws s3 sync "s3://openalex" "$brickpath" --no-sign-request
+aws s3 sync "s3://openalex" "$dlpath" --no-sign-request
 
 echo "Download done."
