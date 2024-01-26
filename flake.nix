67b12c1d448a459191353d04f082363b531e3060
John Shaffer <john@insilica.co>
2024-01-26 14:55:46 -0600

    Add pg2sqlite

--- flake.nix
+++ flake.nix
@@ -9,15 +9,34 @@
 
   outputs = { self, nixpkgs, flake-utils, dev-shell }:
     flake-utils.lib.eachDefaultSystem (system:
-      with import nixpkgs { inherit system; }; {
+      with import nixpkgs { inherit system; };
+      let
+        pg2sqlite = stdenv.mkDerivation rec {
+          pname = "pg2sqlite";
+          version = "1.0.3";
+
+          src = fetchurl {
+            url =
+              "https://github.com/caiiiycuk/postgresql-to-sqlite/releases/download/v${version}/${pname}-${version}.jar";
+            sha256 = "sha256-s7ELwadymvZj3oBUH++OtiwswR5wf1j6izUN4Vz4OE8=";
+          };
+
+          buildInputs = [ makeWrapper jre ];
+
+          dontBuild = true;
+          dontUnpack = true;
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src $out/pg2sqlite-${version}.jar
+            makeWrapper ${jre}/bin/java $out/bin/pg2sqlite \
+              --add-flags "-jar $out/pg2sqlite-${version}.jar"
+          '';
+        };
+      in {
         devShells.default = mkShell {
           inputsFrom = [ dev-shell.devShells.${system}.default ];
-          packages = [
-            /* Problem: awscli uses Python which causes an issue with using
-             * biobricks Python installed outside of shell
-             */
-            #awscli
-          ];
+          packages = [ pg2sqlite ];
         };
       });
 }
